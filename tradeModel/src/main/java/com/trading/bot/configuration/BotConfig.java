c771887f492842fe429dbb59e72c1cea99a4f144
Oleksii Osadchyi <oleksii.osadchyi@iohk.io>
2024-01-22 14:42:32 +0200

    Add ByBIT

--- tradeModel/src/main/java/com/trading/bot/configuration/BotConfig.java
+++ tradeModel/src/main/java/com/trading/bot/configuration/BotConfig.java
@@ -4,6 +4,18 @@ import com.amazonaws.AmazonServiceException;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.bybit.api.client.domain.CategoryType;
+import com.bybit.api.client.domain.market.IntervalTime;
+import com.bybit.api.client.domain.market.MarketInterval;
+import com.bybit.api.client.domain.market.request.MarketDataRequest;
+import com.bybit.api.client.domain.market.response.kline.MarketKlineEntry;
+import com.bybit.api.client.domain.market.response.kline.MarketKlineResult;
+import com.bybit.api.client.domain.market.response.openInterests.OpenInterestEntry;
+import com.bybit.api.client.domain.market.response.openInterests.OpenInterestResult;
+import com.bybit.api.client.restApi.BybitApiAsyncMarketDataRestClient;
+import com.bybit.api.client.restApi.BybitApiMarketRestClient;
+import com.bybit.api.client.service.BybitApiClientFactory;;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.FilenameUtils;
 import org.deeplearning4j.core.storage.StatsStorage;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
@@ -14,13 +26,7 @@ import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.ui.api.UIServer;
 import org.deeplearning4j.ui.model.stats.StatsListener;
-import org.deeplearning4j.ui.model.storage.InMemoryStatsStorage;
-import org.knowm.xchange.Exchange;
-import org.knowm.xchange.ExchangeFactory;
-import org.knowm.xchange.ExchangeSpecification;
-import org.knowm.xchange.currency.CurrencyPair;
-import org.knowm.xchange.kucoin.KucoinExchange;
-import org.knowm.xchange.kucoin.dto.response.KucoinKline;
+import org.deeplearning4j.ui.model.storage.InMemoryStatsStorage;;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
@@ -36,48 +42,34 @@ import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static com.trading.bot.util.TradeUtil.calcData;
 import static com.trading.bot.util.TradeUtil.getDelta;
-import static com.trading.bot.util.TradeUtil.getKucoinKlines;
-import static org.knowm.xchange.kucoin.dto.KlineIntervalType.min15;
+import static java.util.Objects.nonNull;
 
 @Configuration
 public class BotConfig {
     protected final Logger logger = LoggerFactory.getLogger(getClass().getName());
-    public static final int INPUT_SIZE = 5;
+    public static final int INPUT_SIZE = 6;
     public static final int LAYER_SIZE = 24;
     public static final int OUTPUT_SIZE = 3;
     public static final int TRAIN_EXAMPLES = 8;
     public static final int TRAIN_KLINES = 672;
     public static final int PREDICT_DEEP = 8;
     public static final float PRICE_PERCENT = 1.6F;
+    final static String API_KEY = "tAOkQmuG2EUz1stdlUDpDyJRXp8oAY59W1A9Hv4HeR349jfPJbl6Hxuz4gHWKN4I";
+    final static String API_SECRET = "QzJmDQtT02xhxLxrN5irmCtPWxQgJjyMIOQb5AqXXWPQRb9jQQtjrGy36tpkmvJF";
+    public static final String SYMBOL = "SOLUSDT";
+    private static final ObjectMapper MAPPER = new ObjectMapper();
 
-    @Value("${model.bucket}")
+    @Value("${model.bucket:trade-crypto-models}")
     public String bucketName;
-    @Value("${exchange.username}")
-    public String exchangeUserName;
-    @Value("${exchange.apikey}")
-    public String exchangeApiKey;
-    @Value("${exchange.secretkey}")
-    public String exchangeSecretKey;
-    @Value("${exchange.passphrase}")
-    public String exchangePassphrase;
-    public static final CurrencyPair CURRENCY_PAIR = new CurrencyPair("SOL", "USDT");
-
-    @Bean
-    public Exchange getXChangeExchange() {
-        ExchangeSpecification exchangeSpecification = new ExchangeSpecification(KucoinExchange.class);
-
-        exchangeSpecification.setUserName(exchangeUserName);
-        exchangeSpecification.setApiKey(exchangeApiKey);
-        exchangeSpecification.setSecretKey(exchangeSecretKey);
-        exchangeSpecification.setExchangeSpecificParametersItem("passphrase", exchangePassphrase);
-
-        return ExchangeFactory.INSTANCE.createExchange(exchangeSpecification);
-    }
 
     @Bean
     public MultiLayerConfiguration getConfig() {
@@ -108,9 +100,9 @@ public class BotConfig {
     }
 
     @Bean
-    public MultiLayerNetwork getModel(Exchange exchange, MultiLayerConfiguration config, StatsStorage statsStorage)
+    public MultiLayerNetwork getModel(MultiLayerConfiguration config, StatsStorage statsStorage)
             throws IOException {
-        final String keyName = CURRENCY_PAIR.base + ".zip";
+        final String keyName = SYMBOL + ".zip";
         final String path = FilenameUtils.concat(System.getProperty("java.io.tmpdir"), keyName);
         final LocalDateTime now = LocalDateTime.of(2023, 12,1, 0, 0);
         final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.EU_CENTRAL_1).build();
@@ -119,29 +111,51 @@ public class BotConfig {
         net.init();
         net.setListeners(new StatsListener(statsStorage));
 
+        BybitApiMarketRestClient client = BybitApiClientFactory.newInstance().newMarketDataRestClient();
+
+        MarketDataRequest marketDataRequest = MarketDataRequest.builder()
+            .category(CategoryType.LINEAR)
+            .symbol(SYMBOL)
+            .marketInterval(MarketInterval.FIFTEEN_MINUTES)
+            .marketIntervalTime(IntervalTime.FIFTEEN_MINUTES)
+            .build();
+        MarketKlineResult marketKlineResult = new MarketKlineResult();
+        OpenInterestResult openInterestResult = new OpenInterestResult();
+        Object response = new Object();
+
         try (INDArray indData = Nd4j.zeros(TRAIN_EXAMPLES, INPUT_SIZE, TRAIN_KLINES);
              INDArray indLabels = Nd4j.zeros(TRAIN_EXAMPLES, OUTPUT_SIZE, TRAIN_KLINES)) {
 
             int i = TRAIN_EXAMPLES - 1;
             while (i >= 0) {
-                LocalDateTime startDate = now.minusSeconds(
-                        i * (long) TRAIN_KLINES * min15.getSeconds()
-                                + TRAIN_KLINES * min15.getSeconds());
-                LocalDateTime endDate = now.minusSeconds(
-                        i * (long) TRAIN_KLINES * min15.getSeconds()
-                                - PREDICT_DEEP * min15.getSeconds());
+                LocalDateTime startDate = now.minusSeconds(i * (long) TRAIN_KLINES * 15 * 60 + (TRAIN_KLINES + PREDICT_DEEP) * 15 * 60);
 
-                List<KucoinKline> klines =
-                        getKucoinKlines(
-                                exchange,
-                                startDate.toEpochSecond(ZoneOffset.UTC),
-                                endDate.toEpochSecond(ZoneOffset.UTC));
+                List<MarketKlineEntry> klines = new ArrayList<>();
+                marketDataRequest.setStart(startDate.toEpochSecond(ZoneOffset.UTC) * 1000);
+                marketDataRequest.setLimit(PREDICT_DEEP + 96 * 7 + PREDICT_DEEP);
+                response = client.getMarketLinesData(marketDataRequest);
+                if (nonNull(response) && response instanceof Map) {
+                    marketKlineResult = MAPPER.convertValue(((Map) response).get("result"), MarketKlineResult.class);
+                    klines.addAll(marketKlineResult.getMarketKlineEntries());
+                }
+
+                List<OpenInterestEntry> openInterests = new ArrayList<>();
+                marketDataRequest.setStartTime(startDate.toEpochSecond(ZoneOffset.UTC) * 1000);
+                marketDataRequest.setLimit(100);
+                for (int j = 0; j < 7; j++) {
+                    response = client.getOpenInterest(marketDataRequest);
+                    if (nonNull(response) && response instanceof Map) {
+                        openInterestResult = MAPPER.convertValue(((Map) response).get("result"), OpenInterestResult.class);
+                        openInterests.addAll(openInterestResult.getOpenInterestEntries());
+                        marketDataRequest.setCursor(openInterestResult.getNextPageCursor());
+                    }
+                }
+
                 Collections.reverse(klines);
-
-                logger.info("startDate {} endDate {}", startDate, endDate);
+                Collections.reverse(openInterests);
 
                 for (int y = 0; y < TRAIN_KLINES; y++) {
-                    calcData(indData, klines.get(y), i, y);
+                    calcData(indData, i, klines, openInterests, y);
                     indLabels.putScalar(new int[]{i, getDelta(klines, y), y}, 1);
                 }
 
@@ -155,10 +169,9 @@ public class BotConfig {
         }
 
         try {
-            String fileName = CURRENCY_PAIR.base + ".zip";
             net.save(new File(path));
 
-            s3.putObject(bucketName, fileName, new File(path));
+            s3.putObject(bucketName, keyName, new File(path));
         } catch (AmazonServiceException e) {
             logger.error(e.getErrorMessage());
         }
--- tradeModel/src/main/java/com/trading/bot/util/TradeUtil.java
+++ tradeModel/src/main/java/com/trading/bot/util/TradeUtil.java
@@ -1,85 +1,70 @@
 package com.trading.bot.util;
 
+import com.bybit.api.client.domain.market.response.kline.MarketKlineEntry;
+import com.bybit.api.client.domain.market.response.openInterests.OpenInterestEntry;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
-import org.knowm.xchange.Exchange;
-import org.knowm.xchange.kucoin.KucoinMarketDataService;
-import org.knowm.xchange.kucoin.dto.response.KucoinKline;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
-import org.ta4j.core.BarSeries;
 
-import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.time.Duration;
 import java.time.Instant;
 import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
 import java.util.*;
 
 import static com.trading.bot.configuration.BotConfig.*;
-import static org.knowm.xchange.kucoin.dto.KlineIntervalType.min15;
 
 public class TradeUtil {
 
     private TradeUtil() {
     }
 
-    public static List<KucoinKline> getKucoinKlines(Exchange exchange, long startDate, long endDate) throws IOException {
-        return ((KucoinMarketDataService) exchange.getMarketDataService())
-                .getKucoinKlines(CURRENCY_PAIR, startDate, endDate, min15);
-    }
-
-    public static float[] getPredict(KucoinKline kline, MultiLayerNetwork net) {
+    public static float[] getPredict(List<MarketKlineEntry> klines, List<OpenInterestEntry> openInterest, MultiLayerNetwork net) {
         try (INDArray indData = Nd4j.zeros(1, INPUT_SIZE, 1)) {
 
-            calcData(indData, kline, 0, 0);
+            calcData(indData, 0, klines, openInterest, 0);
 
             return net.rnnTimeStep(indData).ravel().toFloatVector();
         }
     }
 
-    public static int getDelta(List<KucoinKline> klines, int i) {
-        BigDecimal delta = klines.get(i + PREDICT_DEEP).getClose()
-            .divide(klines.get(i).getClose(), 6, RoundingMode.HALF_UP)
-            .subtract(BigDecimal.ONE)
-            .multiply(BigDecimal.valueOf(100));
+    public static int getDelta(List<MarketKlineEntry> klines, int i) {
+        BigDecimal minValue = klines.subList(i, i + PREDICT_DEEP * 2).stream()
+            .map(entry -> new BigDecimal(entry.getOpenPrice()))
+            .min(BigDecimal::compareTo)
+            .orElse(BigDecimal.ZERO);
 
-        if (delta.floatValue() > PRICE_PERCENT) {
-            return  2;
-        } else if (delta.floatValue() < -PRICE_PERCENT / 2) {
-            return  0;
+        BigDecimal maxValue = klines.subList(i, i + PREDICT_DEEP * 2).stream()
+            .map(entry -> new BigDecimal(entry.getOpenPrice()))
+            .max(BigDecimal::compareTo)
+            .orElse(BigDecimal.ZERO);
+
+        if (minValue.equals(new BigDecimal(klines.get(i + PREDICT_DEEP).getOpenPrice()))) {
+            return 2;
+        } else if (maxValue.equals(new BigDecimal(klines.get(i + PREDICT_DEEP).getOpenPrice()))) {
+            return 0;
         } else {
             return 1;
         }
     }
 
-    public static void calcData(INDArray indData, KucoinKline kline, int i, int y) {
-        indData.putScalar(new int[]{i, 0, y},
-                          kline.getClose().subtract(kline.getOpen()).floatValue() / kline.getClose().floatValue() * 10F);
-        indData.putScalar(new int[]{i, 1, y},
-                          kline.getAmount().floatValue() / 10_000_000F);
-        indData.putScalar(new int[]{i, 2, y},
-                          kline.getClose().compareTo(kline.getOpen()) > 0 ?
-                          kline.getHigh().subtract(kline.getClose()).floatValue() / kline.getClose().floatValue() * 10F :
-                          kline.getHigh().subtract(kline.getOpen()).floatValue() / kline.getClose().floatValue() * 10F);
-        indData.putScalar(new int[]{i, 3, y},
-                          kline.getClose().compareTo(kline.getOpen()) > 0 ?
-                          kline.getOpen().subtract(kline.getLow()).floatValue() / kline.getClose().floatValue() * 10F :
-                          kline.getClose().subtract(kline.getLow()).floatValue() / kline.getClose().floatValue() * 10F);
-        indData.putScalar(new int[]{i, 4, y},
-                          Instant.ofEpochSecond(kline.getTime()).atOffset(ZoneOffset.UTC).getDayOfWeek().getValue() * 0.12F
-                          + Instant.ofEpochSecond(kline.getTime()).atOffset(ZoneOffset.UTC).getHour() * 0.005F);
-    }
+    public static void calcData(INDArray indData, int i, List<MarketKlineEntry> kline, List<OpenInterestEntry> openInterest, int y) {
+        float open = Float.parseFloat(kline.get(y + PREDICT_DEEP).getOpenPrice());
+        float high = Float.parseFloat(kline.get(y + PREDICT_DEEP).getHighPrice());
+        float low = Float.parseFloat(kline.get(y + PREDICT_DEEP).getLowPrice());
+        float close = Float.parseFloat(kline.get(y + PREDICT_DEEP).getClosePrice());
+        float value = Float.parseFloat(kline.get(y + PREDICT_DEEP).getTurnover());
+        float day = Instant.ofEpochMilli(kline.get(y + PREDICT_DEEP).getStartTime()).atOffset(ZoneOffset.UTC).getDayOfWeek().getValue() * 0.12F;
+        float hour = Instant.ofEpochMilli(kline.get(y + PREDICT_DEEP).getStartTime()).atOffset(ZoneOffset.UTC).getHour() * 0.005F;
+        float prevInterest = Float.parseFloat(openInterest.get(y + PREDICT_DEEP - 1).getOpenInterest());
+        float interest = Float.parseFloat(openInterest.get(y + PREDICT_DEEP).getOpenInterest());
 
-    public static void loadBarSeries(BarSeries barSeries, KucoinKline kucoinKlines) {
-        barSeries.addBar(Duration.ofMinutes(5L),
-                         ZonedDateTime.ofInstant(Instant.ofEpochSecond(kucoinKlines.getTime()), ZoneOffset.UTC),
-                         kucoinKlines.getOpen(),
-                         kucoinKlines.getHigh(),
-                         kucoinKlines.getLow(),
-                         kucoinKlines.getClose(),
-                         kucoinKlines.getVolume());
+        indData.putScalar(new int[]{i, 0, y}, (close - open) / close * 10F);
+        indData.putScalar(new int[]{i, 1, y}, value / 10_000_000F);
+        indData.putScalar(new int[]{i, 2, y}, close > open ? (high - close) / close * 10F : (high - open) / close * 10F);
+        indData.putScalar(new int[]{i, 3, y}, close > open ? (open - low) / close * 10F : (close - low) / close * 10F);
+        indData.putScalar(new int[]{i, 4, y}, day + hour);
+        indData.putScalar(new int[]{i, 5, y}, (prevInterest - interest) * 0.00001F);
     }
 
     public static String printRates(float[] floatResult) {
